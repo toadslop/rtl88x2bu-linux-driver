bba5e0196406880d0d8f2653c44fe68b0a7eea67
Brian <31160338+toadslop@users.noreply.github.com>
2026-07-27 14:43:52 +0900

    [5/5] W3-09b: Port wlan_util network type and MCS helpers to Rust (#165)

--- tests/host/wlan_util/wlan_util_c_oracle.c
+++ tests/host/wlan_util/wlan_util_c_oracle.c
@@ -3,7 +3,9 @@
  * C oracle slice: core/rtw_wlan_util.c rate-classification helpers (T5/W3-08).
  */
 #include <stdbool.h>
+#include <string.h>
 
+#include "host_chplan_types.h"
 #include "host_wlan_util_types.h"
 
 u8 WIFI_CCKRATES[] = {
@@ -224,4 +226,65 @@ unsigned int host_ratetbl2rateset(host_wlan_adapter *padapter,
 		}
 	}
 	return len;
+}
+
+/* ----- network type + MCS helpers (W3-09b) ----- */
+
+typedef struct {
+	char _pad0[0x844];
+	u8 cur_channel;
+	char _pad1[0xc58 - 0x844 - 1];
+	u8 ht_enable;
+	char _pad2[0xc66 - 0xc58 - 1];
+	u8 vht_enable;
+} host_network_adapter;
+
+u8 judge_network_type(host_network_adapter *padapter, unsigned char *rate,
+		      int ratelen)
+{
+	u8 network_type = 0;
+
+	if (padapter->cur_channel > 14) {
+		if (padapter->vht_enable)
+			network_type = WIRELESS_11AC;
+		else if (padapter->ht_enable)
+			network_type = WIRELESS_11_5N;
+
+		network_type |= WIRELESS_11A;
+	} else {
+		if (padapter->ht_enable)
+			network_type = WIRELESS_11_24N;
+
+		if (cckratesonly_included(rate, ratelen) == _TRUE)
+			network_type |= WIRELESS_11B;
+		else if (cckrates_included(rate, ratelen) == _TRUE)
+			network_type |= WIRELESS_11BG;
+		else
+			network_type |= WIRELESS_11G;
+	}
+
+	return network_type;
+}
+
+void get_rate_set(host_wlan_adapter *padapter, unsigned char *pbssrate,
+		  int *bssrate_len)
+{
+	unsigned char supportedrates[NumRates];
+
+	memset(supportedrates, 0, sizeof(supportedrates));
+	*bssrate_len = (int)host_ratetbl2rateset(padapter, supportedrates);
+	memcpy(pbssrate, supportedrates, (size_t)*bssrate_len);
+}
+
+void set_mcs_rate_by_mask(u8 *mcs_set, u32 mask)
+{
+	u8 mcs_rate_1r = (u8)(mask & 0xff);
+	u8 mcs_rate_2r = (u8)((mask >> 8) & 0xff);
+	u8 mcs_rate_3r = (u8)((mask >> 16) & 0xff);
+	u8 mcs_rate_4r = (u8)((mask >> 24) & 0xff);
+
+	mcs_set[0] &= mcs_rate_1r;
+	mcs_set[1] &= mcs_rate_2r;
+	mcs_set[2] &= mcs_rate_3r;
+	mcs_set[3] &= mcs_rate_4r;
 }
