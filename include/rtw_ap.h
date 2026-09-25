94662372edf18fab8aceb420c696d270516cc28d
Brian <31160338+toadslop@users.noreply.github.com>
2026-09-25 15:57:58 +0900

    ap: extract update_bcn_wps_ie to rtw_ap_bcn_update.c (#418) (#836)

--- include/rtw_ap.h
+++ include/rtw_ap.h
@@ -34,6 +34,7 @@ u8 rtw_ap_release_vapid(struct dvobj_priv *dvobj, u8 vap_id);
 void update_BCNTIM(_adapter *padapter);
 void update_bcn_erpinfo_ie(_adapter *padapter);
 void update_bcn_htinfo_ie(_adapter *padapter);
+void update_bcn_wps_ie(_adapter *padapter);
 void rtw_add_bcn_ie(_adapter *padapter, WLAN_BSSID_EX *pnetwork, u8 index, u8 *data, u8 len);
 void rtw_remove_bcn_ie(_adapter *padapter, WLAN_BSSID_EX *pnetwork, u8 index);
 void _update_beacon(_adapter *padapter, u8 ie_id, u8 *oui, u8 tx, u8 flags, const char *tag);
